730b78144a59e5d0e6172f88a53ecb8c1a15af63
tsound922 <oushirx@gmail.com>
2018-11-13 01:29:45 +1100

    Angular playground

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import {HEROS} from '../mock-heroes';
+ 
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  heroes = HEROS;
+  
+  
+
+  
+ 
+  constructor() { }
+ 
+  ngOnInit() {
+  }
+ 
+}
